cf5f8b1fc372e6fcb72967ce6783ae5ab697f992
Alaa Hassan <alaa.hassan200362@gmail.com>
2026-03-06 02:41:25 +0200

    style: apply clang-format to cluster bridge files

--- src/cluster_bridge/include/FrameStates.h
+++ src/cluster_bridge/include/FrameStates.h
@@ -5,23 +5,24 @@
 #ifndef VEHICLEAUTOWAREAGENT_FRAMESTATES_H
 #define VEHICLEAUTOWAREAGENT_FRAMESTATES_H
 #include <cstdint>
+
 #include <vehicle_frame.grpc.pb.h>
 
 struct FrameState {
   float speed_mps{0.0}, speed_kmh{0.0};
   vehicle_frame::GearState gear{vehicle_frame::GEAR_UNKNOWN};
   float steering_angle_deg{0.0};
-  bool hazard_on {false};
+  bool hazard_on{false};
   vehicle_frame::TurnSignal turn_signal{vehicle_frame::TURN_NONE};
   vehicle_frame::ControlMode control_mode{vehicle_frame::MODE_UNKNOWN};
-  float  battery_pct {0.0};
+  float battery_pct{0.0};
   float accel_mps2{0.0};
   float yaw_rate{0.0};
-  vehicle_frame::MotionState motion_state{vehicle_frame::MOTION_UNKNOWN };
+  vehicle_frame::MotionState motion_state{vehicle_frame::MOTION_UNKNOWN};
   float target_speed_mps{0.0};
   float speed_limit_mps{0.0};
-  float remaining_distance_m {0.0};
-  float remaining_time_s     {0.0};
+  float remaining_distance_m{0.0};
+  float remaining_time_s{0.0};
   bool traffic_light_red{false};
   bool traffic_light_yellow{false};
   bool traffic_light_green{false};
@@ -30,8 +31,7 @@ struct FrameState {
   uint32_t vehicle_count{0};
   std::vector<vehicle_frame::SurroundObject> surround_objects{};
   vehicle_frame::MrmBehavior mrm_behavior{vehicle_frame::MRM_NONE};
-  bool is_active {false};
-  std::string description {""};
-
+  bool is_active{false};
+  std::string description{""};
 };
 #endif  // VEHICLEAUTOWAREAGENT_FRAMESTATES_H
