34604c7b05b88d57ff7b998d841a8a0421f53c2a
Alaa Hassan <alaa.hassan200362@gmail.com>
2026-04-02 19:24:43 +0200

    starting the trip bridge at the main

--- src/main.cc
+++ src/main.cc
@@ -17,8 +17,8 @@
 #include "AutowareController.h"
 #include "Config.h"
 #include "cluster_bridge/include/ClusterBridge.h"
-#include "planning_bridge/include/PlanningBridge.h"
 #include "perception_bridge/include/PerceptionBridge.h"
+#include "planning_bridge/include/PlanningBridge.h"
 #include "trip_bridge/include/TripBridge.h"
 
 
